1a95ef27dd57de332c80b626e4c76697de5aeca9
migueldlozano <miguel.d.lozano@gmail.com>
2016-11-25 12:31:28 -0500

    Modify package.json and server.js for production

--- react-weather/server.js
+++ react-weather/server.js
@@ -1,8 +1,17 @@
 var express = require('express'),
-    port = 3000, // Specify Express server port
     app = express(); // Create app
+const PORT = process.env.PORT || 3000, // Specify Express server port
+
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 
 app.use(express.static('public'));
-app.listen(port, function() {
-  console.log('Express Server is running on port ' + port);
+app.listen(PORT, function() {
+  console.log('Express Server is running on port ' + PORT);
 });
