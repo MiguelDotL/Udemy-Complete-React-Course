3ec48ef016cfda0f55c8967e48270facb80ed684
migueldlozano <miguel.d.lozano@gmail.com>
2016-12-01 17:49:21 -0500

    Add React Timer and Basic Nav Styles

---
+++ react-timer/app/components/Main.jsx
@@ -0,0 +1,20 @@
+// import React, { Component } from 'react';
+//
+// export default class Main extends Component {
+//
+// }
+
+var React = require('react'),
+
+    Nav = require('Nav'),
+
+    Main = (props) => {
+      return(
+        <div>
+          <Nav />
+
+        </div>
+      );
+    };
+
+module.exports = Main;
